5e5b58a608aadda43ff62e8dbf96a656731509b2
felisianovitasari58 <felisianovitasari@yahoo.co.id>
2018-03-25 19:22:56 +0700

    Four Of Kind SUKSES

--- FourOfKind.py
+++ FourOfKind.py
@@ -1,4 +1,4 @@
-# cara satu
+
 increment=0
 kartu=["2w","2k","2h","2s","3w","3k","3h","3s","4w","4k","4h","4s","5w","5k","5h","5s","6w","6k","6h","6s","7w","7k","7h","7s","8w","8k","8h","8s","9w","9k","9h","9s","10w","10k","10h","10s","Jw","Jk","Jh","Js","Qw","Qk","Qh","Qs","Kw","Kk","Kh","Ks","Aw","Ak","Ah","As"]
 arrtemp=[]
